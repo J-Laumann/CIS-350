54f50f93333dfbe8b6328c198aea86f2856ed141
Jackson Laumann <jlaumann24@gmail.com>
2022-10-23 23:33:33 -0500

    3D Prototype

---
+++ 3D-Prototype/Assets/MyFirstPersonPlayer/Scripts/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    public GameObject endText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            endText.SetActive(true);
+        }
+    }
+}
---
+++ 3D-Prototype/Assets/MyFirstPersonPlayer/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+    public GameObject player;
+    public float verticalLookRotation = 0f;
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        player.transform.Rotate(Vector3.up * mouseX);
+
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(verticalLookRotation, 0, 0);
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
